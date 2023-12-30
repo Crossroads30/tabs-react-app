741ffc150fa79cc3d1bc1d16c047c6eb0f5476a6
crossroads30 <crossroads30@mail.ru>
2023-12-31 00:34:30 +0300

    feat: add mapping for data, add 'more info btn'

--- src/App.jsx
+++ src/App.jsx
@@ -44,10 +44,36 @@ function App() {
 	if (isError) {
 		return <h3>There was an error!</h3>
 	}
-  
+
 	return (
 		<main>
-			<section className='section'></section>
+			<section className='section'>
+				<div className='title'>
+					<h2>Experience</h2>
+					<div className='underline'></div>
+				</div>
+				<div className='jobs-center'>
+					<div className='btn-container'>
+						{jobs.map(job => {
+							return (
+								<button className='job-btn active-btn' type='button'>
+									{job.company}
+								</button>
+							)
+						})}
+					</div>
+					<article className='job-info'>
+						<h3>sfddfd</h3>
+						<h4>sdfsdfsd</h4>
+						<p className='job-date'>dfdfdfd</p>
+						<div className='job-desc'>
+							<FaAngleDoubleRight className='job-icon' />
+							<p>dfvdfv</p>
+						</div>
+					</article>
+				</div>
+        <button className='btn'>more info</button>
+			</section>
 		</main>
 	)
 }
