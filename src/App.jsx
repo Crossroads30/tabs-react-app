180e069e75614053820ac9b6487e9ce95f5e0b6a
crossroads30 <crossroads30@mail.ru>
2023-12-31 00:10:15 +0300

    feat: add fetching data func, add error handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
+	const [loading, setLoading] = useState(true)
+	const [jobs, setJobs] = useState([])
+	const [isError, setIsError] = useState(false)
+
+	const fetchingData = async () => {
+		try {
+			const response = await fetch(url)
+			if (!response.ok) {
+				// case for 'fetch' to handle '400th' & '500th' errors!
+				setIsError(true)
+				setLoading(false)
+				return
+			}
+			const jobs = await response.json()
+			console.log(jobs)
+			setLoading(false)
+			setJobs(jobs)
+		} catch (error) {
+			console.log(error)
+			setIsError(true)
+		}
+		setLoading(false)
+	}
+
+	useEffect(() => {
+		fetchingData()
+	}, [])
+
+	if (loading) {
+		return (
+			<main>
+				<section className='section'>
+					<h3 className='loading'>Loading...</h3>
+				</section>
+			</main>
+		)
+	}
+	if (isError) {
+		return <h3>There was an error!</h3>
+	}
+  
+	return (
+		<main>
+			<section className='section'></section>
+		</main>
+	)
 }
 
 export default App
